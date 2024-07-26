d69001652efa56963e5f5f6355edea79ec51bbcf
David Snyder <davidsnyder.jet@gmail.com>
2024-07-26 12:36:28 -0500

    day 8 part 2

--- day_8/treetop_tree_house.py
+++ day_8/treetop_tree_house.py
@@ -5,7 +5,6 @@ class TreetopTreeHouse:
     lines = data.splitlines()
     trees = [[*r] for r in lines]
     visible_count = 0
-#    print(data)
     for x in range(len(trees)):
       for y in range(len(trees[0])):
         if x == 0 or y == 0 or x == len(trees)-1 or y == len(trees[0])-1:
@@ -13,32 +12,58 @@ class TreetopTreeHouse:
         else:
           visible_e,visible_s,visible_n,visible_w = [True,True,True,True]
           for i in range(x+1, len(trees)):
-#            print(x,y,i,y)            
             if trees[i][y] >= trees[x][y]:
               visible_s = False
               break
           for i in range(0, x):
-#            print(x,y,i,y)            
             if trees[i][y] >= trees[x][y]:
               visible_n = False
               break
           for j in range(y+1, len(trees[0])):
-#            print(x,y,x,j)            
             if trees[x][j] >= trees[x][y]:
               visible_e = False
               break
           for j in range(0, y):
-#            print(x,y,x,j)                          
             if trees[x][j] >= trees[x][y]:
               visible_w = False
               break
           if visible_n or visible_e or visible_s or visible_w:
-#            print(x,y,"visible")
             visible_count += 1
     return visible_count
 
+  def scenic_score(self, data):
+    lines = data.splitlines()
+    trees = [[*r] for r in lines]
+    scenic_score = 0
+    for x in range(len(trees)):
+      for y in range(len(trees[0])):
+        if x == 0 or y == 0 or x == len(trees)-1 or y == len(trees[0])-1:
+          pass
+        else:
+          visible_e,visible_s,visible_n,visible_w = [0,0,0,0]
+          for i in range(x+1, len(trees)):
+            visible_s +=1                        
+            if trees[i][y] >= trees[x][y]:
+              break
+          for i in range(x-1,-1,-1):
+            visible_n +=1                                    
+            if trees[i][y] >= trees[x][y]:
+              break
+          for j in range(y+1, len(trees[0])):
+            visible_e +=1                                    
+            if trees[x][j] >= trees[x][y]:
+              break
+          for j in range(y-1,-1,-1):
+            visible_w +=1                                    
+            if trees[x][j] >= trees[x][y]:
+              break
+          scenic_score = max(scenic_score,visible_n * visible_s * visible_w * visible_e)
+    return scenic_score
+
 def main():
   sol = TreetopTreeHouse()
-  print(str(sol.num_visible(sys.stdin.read())))
+  input = sys.stdin.read()
+  print("part 1: " + str(sol.num_visible(input)))  
+  print("part 2: " + str(sol.scenic_score(input)))
 
 main()
